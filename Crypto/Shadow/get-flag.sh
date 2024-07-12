9908e5dada963fd7f0a7bfae96b91a66786d56a7
Euthanazepam <euthanazepam@protonmail.com>
2024-07-12 15:23:41 +0000

    #update #crypto - shadow, checking existence of rockyou.txt

--- Crypto/Shadow/get-flag.sh
+++ Crypto/Shadow/get-flag.sh
@@ -3,15 +3,16 @@
 # https://w3challs.com/challenges/crypto/shadow
 
 function downloadAndExtractDictionary() {
+  if [ ! -f rockyou.txt ]; then
     wget https://raw.githubusercontent.com/zacheller/rockyou/master/rockyou.txt.tar.gz
     tar -xf rockyou.txt.tar.gz
-
-    # wget https://raw.githubusercontent.com/josuamarcelc/common-password-list/main/rockyou.txt/rockyou.txt.zip
-    # unzip rockyou.txt.zip
+#    wget https://raw.githubusercontent.com/josuamarcelc/common-password-list/main/rockyou.txt/rockyou.txt.zip
+#    unzip rockyou.txt.zip
+  fi
 }
 
 function decryptHash() {
-    hashcat -m 500 -a 0 shadow rockyou.txt
+  hashcat -m 500 -a 0 shadow rockyou.txt
 }
 
 downloadAndExtractDictionary
