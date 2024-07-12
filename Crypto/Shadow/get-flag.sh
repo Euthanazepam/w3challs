746ad662ca1ee05acbc6369c6aaeac40ecb00ea0
Euthanazepam <euthanazepam@protonmail.com>
2024-07-12 23:10:23 +0000

    #update #crypto - shadow, removing .potfile

--- Crypto/Shadow/get-flag.sh
+++ Crypto/Shadow/get-flag.sh
@@ -2,8 +2,18 @@
 
 # https://w3challs.com/challenges/crypto/shadow
 
+DICTIONARY=rockyou.txt
+FILE=shadow
+POTFILE=~/.local/share/hashcat/hashcat.potfile
+
+function removePotFile() {
+  if [ -f $POTFILE ]; then
+    rm $POTFILE
+  fi
+}
+
 function downloadAndExtractDictionary() {
-  if [ ! -f rockyou.txt ]; then
+  if [ ! -f $DICTIONARY ]; then
     wget https://raw.githubusercontent.com/zacheller/rockyou/master/rockyou.txt.tar.gz
     tar -xf rockyou.txt.tar.gz
 #    wget https://raw.githubusercontent.com/josuamarcelc/common-password-list/main/rockyou.txt/rockyou.txt.zip
@@ -12,8 +22,10 @@ function downloadAndExtractDictionary() {
 }
 
 function decryptHash() {
-  hashcat -m 500 -a 0 shadow rockyou.txt
+  hashcat -m 500 -a 0 $FILE $DICTIONARY
+  cat $POTFILE | cut -d : -f 2
 }
 
+removePotFile
 downloadAndExtractDictionary
 decryptHash
